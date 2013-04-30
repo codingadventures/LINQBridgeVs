3df7fbf645089adc2d260b40974278bb978acf32
Giovanni Campo <nbasakuragi@gmail.com>
2013-04-30 21:33:52 +0000

    #1

--- Bridge.Visualizers/LINQPadDebuggerVisualizerObjectSource.cs
+++ Bridge.Visualizers/LINQPadDebuggerVisualizerObjectSource.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using Bridge.Grapple;
 using Bridge.Visualizers.Template;
+using Bridge.Visualizers.Utils;
 using Microsoft.VisualStudio.DebuggerVisualizers;
 
 namespace Bridge.Visualizers
@@ -12,19 +11,21 @@ namespace Bridge.Visualizers
 
     public class LINQPadDebuggerVisualizerObjectSource : VisualizerObjectSource
     {
-        internal const string FileNameFormat = "ddMMyy_Hmmss_{0}.linq";
+        internal const string FileNameFormat = "ddMMyy_{0}.linq";
 
 
         public override void GetData(object target, Stream outgoingData)
         {
             var scriptFileName = DateTime.Now.ToString(FileNameFormat);
+            var targetType = target.GetType();
+            var targetTypeFullName = TypeNameHelper.GetDisplayName(targetType, true);
 
             var message = new Message
                               {
-                                  FileName = string.Format(scriptFileName, target.GetType().FullName),
-                                  TypeFullName = target.GetType().FullName,
-                                  TypeLocation = target.GetType().Assembly.Location,
-                                  TypeNamespace = target.GetType().Namespace
+                                  FileName = string.Format(scriptFileName, targetTypeFullName.Replace("<>", "_")),
+                                  TypeFullName = targetTypeFullName,
+                                  TypeLocation = targetType.Assembly.Location,
+                                  TypeNamespace = targetType.Namespace
                               };
 
             var binaryFormatter = new BinaryFormatter();
