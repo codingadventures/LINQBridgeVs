ea7ecea4a901adbd3e2ae2bb24f3c046df2411ad
Giovanni Campo <nbasakuragi@hotmail.com>
2013-05-14 18:23:50 +0100

    added few log messages

--- Bridge.Visualizers/LINQPadDebuggerVisualizer.cs
+++ Bridge.Visualizers/LINQPadDebuggerVisualizer.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Reflection;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
 using Bridge.Visualizers.Properties;
 using Bridge.Visualizers.Template;
 using Microsoft.VisualStudio.DebuggerVisualizers;
+
 
 namespace Bridge.Visualizers
 {
@@ -14,43 +17,65 @@ namespace Bridge.Visualizers
     /// </summary>
     public class LINQPadDebuggerVisualizer : DialogDebuggerVisualizer
     {
-        internal void DeployScripts(Message message)
+
+
+        private static readonly string MyDocuments = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        private static List<string> _logs = new List<string>();
+
+
+        internal static void DeployScripts(Message message)
         {
-
-            var dstScriptPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
-                                             "LINQPad Queries");
-
-            var dst = Path.Combine(dstScriptPath, string.Format(message.FileName, message.TypeFullName));
-            if (File.Exists(dst)) return;
-
-            var linqQuery = new Inspection(new List<string> { message.TypeLocation }, message.TypeFullName, message.TypeNamespace);
-            var linqQueryText = linqQuery.TransformText();
-
-            using (var streamWriter = new StreamWriter(dst, false))
+            try
             {
-                streamWriter.Write(linqQueryText);
-                streamWriter.Flush();
+                _logs.Add(message.ToString());
+                _logs.Add("MyDocuments: " + MyDocuments);
+                _logs.Add("Resources.LINQPadQuery: " + Resources.LINQPadQuery);
+                var dstScriptPath = Path.Combine(MyDocuments, Resources.LINQPadQuery);
+                _logs.Add("dstScriptPath: " + dstScriptPath);
+
+                var dst = Path.Combine(dstScriptPath, string.Format(message.FileName, message.TypeFullName));
+
+
+                if (File.Exists(dst)) return;
+
+                var linqQuery = new Inspection(new List<string> { message.TypeLocation }, message.TypeFullName, message.TypeNamespace);
+                var linqQueryText = linqQuery.TransformText();
+
+                using (var streamWriter = new StreamWriter(dst, false))
+                {
+                    streamWriter.Write(linqQueryText);
+                    streamWriter.Flush();
+                }
+            }
+            catch (Exception e)
+            {
+                _logs.Add(e.Message);
+                _logs.Add(e.StackTrace);
+                Grapple.Bus.Instance.Add(_logs);
+                Grapple.Bus.Instance.BroadCast();
+
+                throw;
             }
         }
 
         protected override void Show(IDialogVisualizerService windowService, IVisualizerObjectProvider objectProvider)
         {
+
             var formatter = new BinaryFormatter();
             var message = (Message)formatter.Deserialize(objectProvider.GetData());
+
             DeployScripts(message);
+
             using (var process = new Process())
             {
-
                 var startInfo = new ProcessStartInfo
                                     {
                                         WindowStyle = ProcessWindowStyle.Normal,
                                         LoadUserProfile = true,
                                         FileName = Environment.GetEnvironmentVariable("LinqPadPath") + @"\" + Resources.LINQPadExe,
-
                                         Arguments =
-                                            Path.Combine(
-                                                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
-                                                Resources.LINQPadQuery, message.FileName) + " " + Resources.LINQPadCommands,
+                                            Path.Combine(MyDocuments, Resources.LINQPadQuery, message.FileName) + " " + Resources.LINQPadCommands,
 
                                     };
 
--- Bridge.Visualizers/Template/Message.cs
+++ Bridge.Visualizers/Template/Message.cs
@@ -1,14 +1,19 @@
 ﻿using System;
- 
+
 
 namespace Bridge.Visualizers.Template
 {
     [Serializable]
-    internal struct  Message
+    internal struct Message
     {
         public string FileName;
         public string TypeFullName;
         public string TypeLocation;
         public string TypeNamespace;
+
+        public override string ToString()
+        {
+            return FileName + " " + TypeFullName + " " + TypeLocation + " " + TypeNamespace;
+        }
     }
 }
