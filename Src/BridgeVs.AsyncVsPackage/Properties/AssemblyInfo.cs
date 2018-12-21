3b4215823a66df7dff0419ea28b15a763953ed5b
codingadventures <linqbridgevs@gmail.com>
2018-12-21 20:37:42 +0000

    adds shared assembly and fixes newtonsoft version

--- Src/BridgeVs.AsyncVsPackage/Properties/AssemblyInfo.cs
+++ Src/BridgeVs.AsyncVsPackage/Properties/AssemblyInfo.cs
@@ -28,32 +28,9 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("BridgeVs.AsyncVsPackage")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")] 
-[assembly: AssemblyCompany("Coding Adventures")]
 [assembly: AssemblyProduct("BridgeVs.VisualStudio.AsyncExtension")]
-[assembly: AssemblyCopyright("Copyright © Coding Adventures 2013 - 2018")]
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: Guid("2016fd5d-79e9-4823-b927-cb796f7b411a")]
-[assembly: AssemblyVersion("2.0.*")]
 #if TEST
 [assembly: InternalsVisibleTo("BridgeVs.UnitTest, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f575ceee4c0b7992660f21a6c2a09c93eac56d9dad2f20caa2d48bf5d904c9b2af5800ba01cae7b37299bff9486a8b97047959c3fbe16de730cf3397f4bafaefc745dba1ce34cedf27698f2dc96159eaa27eef4093f6c35236f30239a4841b864ea734ed3582478cc4214d76497ceb974ac920f35043de0913a149d1107bd3a1")]
 #endif
